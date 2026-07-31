25c2a4b3a2840098e45e174c6de369fc35d392c2
camilodangaud <143123036+camilodangaud@users.noreply.github.com>
2026-07-31 15:12:07 -0500

    connection databases

--- php/config/conection.php
+++ php/config/conection.php
@@ -1,6 +1,12 @@
 <?php
 $localhost = "localhost";
-$username = "root";
-$password = "";
-$database = "bd_hair_salon";
+$username  = "root";
+$password  = "";
+$database  = "bd_hair_salon";
+
+$conn = mysqli_connect($localhost, $username, $password, $database);
+
+if (!$conn) {
+  die("Error de conexión: " . mysqli_connect_error());
+}
 ?>
