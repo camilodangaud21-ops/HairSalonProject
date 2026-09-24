a2131f290142a58344655ac8cd4c8d163a60387c
camilodangaud21-ops <camilodangaud21@gmail.com>
2026-09-24 10:35:54 -0500

    feat: add client account area and drawer

---
+++ php/client/_guard.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+if (!isset($_SESSION['user']) || ($_SESSION['user']['role'] ?? '') !== 'client') {
+    header('Location: ../../index.php');
+    exit;
+}
+$user = $_SESSION['user'];
+?>
